94602be6694a99f50666b468c75902a8cb4eec4a
Nathan Haustveit <nathanhaustveit@gmail.com>
2022-03-29 16:00:47 -0500

    took lecture notes on java strings and completed string and bob exercises

---
+++ src/Bob.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write a statement or ask a question to talk to Bob.");
+
+        String userSentence = sc.nextLine();
+        String question = "?";
+        String exclamation = "!";
+        if (userSentence.endsWith(question)){
+            System.out.println("Sure.");
+        }else if (userSentence.endsWith(exclamation)) {
+            System.out.println("Whoa, chill out!");
+        }else if (userSentence.equals("")){
+            System.out.println("Whatever.");
+        }else{
+            System.out.println("Sure.");
+        }
+        //System.out.println(userSentence);
+    }
+}
